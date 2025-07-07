79e60416109ea9d19f72e21853659b1783a079bf
Willem Jan Glerum <wjglerum@gmail.com>
2025-07-07 16:28:47 +0200

    Basic samples

--- src/main/java/nl/wjglerum/GreetingResource.java
+++ src/main/java/nl/wjglerum/GreetingResource.java
@@ -1,5 +1,8 @@
 package nl.wjglerum;
 
+import io.smallrye.common.annotation.NonBlocking;
+import io.smallrye.common.annotation.RunOnVirtualThread;
+import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -9,8 +12,32 @@ import jakarta.ws.rs.core.MediaType;
 public class GreetingResource {
 
     @GET
+    @Path("/blocking")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String blocking() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("/non-blocking")
+    @NonBlocking
+    @Produces(MediaType.TEXT_PLAIN)
+    public String nonBlocking() {
+        return "Hello from Quarkus REST non blocking";
+    }
+
+    @GET
+    @Path("/reactive")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Uni<String> reactive() {
+        return Uni.createFrom().item("Hello from Quarkus REST reactive");
+    }
+
+    @GET
+    @Path("/virtual-thread")
+    @Produces(MediaType.TEXT_PLAIN)
+    @RunOnVirtualThread
+    public String virtualThread() {
+        return "Hello from Quarkus REST on a virtual thread";
     }
 }

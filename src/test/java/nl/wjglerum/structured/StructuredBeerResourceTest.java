ca67193142545d530474354eb7eba0c1ace27db7
Willem Jan Glerum <wjglerum@gmail.com>
2025-09-16 14:11:18 +0200

    Enable Virtual Thread pinning test

--- src/test/java/nl/wjglerum/structured/StructuredBeerResourceTest.java
+++ src/test/java/nl/wjglerum/structured/StructuredBeerResourceTest.java
@@ -2,17 +2,22 @@ package nl.wjglerum.structured;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit5.virtual.ShouldNotPin;
+import io.quarkus.test.junit5.virtual.VirtualThreadUnit;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
+@VirtualThreadUnit
 @TestHTTPEndpoint(StructuredBeerResource.class)
 class StructuredBeerResourceTest {
 
     @Test
+    @ShouldNotPin
     void testBeerEndpoint() {
         given()
                 .accept(ContentType.JSON)
--- src/test/java/nl/wjglerum/virtual/VirtualBeerResourceTest.java
+++ src/test/java/nl/wjglerum/virtual/VirtualBeerResourceTest.java
@@ -2,16 +2,20 @@ package nl.wjglerum.virtual;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit5.virtual.ShouldNotPin;
+import io.quarkus.test.junit5.virtual.VirtualThreadUnit;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
+@VirtualThreadUnit
 @TestHTTPEndpoint(VirtualBeerResource.class)
 class VirtualBeerResourceTest {
 
     @Test
+    @ShouldNotPin
     void testBeerEndpoint() {
         given()
                 .when().get()

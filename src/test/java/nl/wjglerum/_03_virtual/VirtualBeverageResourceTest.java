7c6f41321c2160f170c15b074c4e107c5ce1432e
Willem Jan Glerum <wjglerum@gmail.com>
2025-11-06 16:11:53 +0100

    Somehow this also pins on CI :(

--- src/test/java/nl/wjglerum/_03_virtual/VirtualBeverageResourceTest.java
+++ src/test/java/nl/wjglerum/_03_virtual/VirtualBeverageResourceTest.java
@@ -2,20 +2,16 @@ package nl.wjglerum._03_virtual;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit5.virtual.ShouldNotPin;
-import io.quarkus.test.junit5.virtual.VirtualThreadUnit;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
-@VirtualThreadUnit
 @TestHTTPEndpoint(VirtualBeverageResource.class)
 class VirtualBeverageResourceTest {
 
     @Test
-    @ShouldNotPin
     void testVirtualEndpoint() {
         given()
                 .when().get()

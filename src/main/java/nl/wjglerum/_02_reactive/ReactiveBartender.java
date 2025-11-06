44c026085f0c4f678efdc374fa015e3dc9628064
Willem Jan Glerum <wjglerum@gmail.com>
2025-11-06 16:07:58 +0100

    Delay uni and delegate to scheduled executor

--- src/main/java/nl/wjglerum/_02_reactive/ReactiveBartender.java
+++ src/main/java/nl/wjglerum/_02_reactive/ReactiveBartender.java
@@ -2,13 +2,21 @@ package nl.wjglerum._02_reactive;
 
 import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+import io.smallrye.mutiny.vertx.core.ContextAwareScheduler;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.time.Duration;
 
 @ApplicationScoped
 public class ReactiveBartender {
 
     public Uni<ReactiveBeverage> getFromDraft() {
         Log.info("Going to pour a reactive Guinness");
-        return Uni.createFrom().item(new ReactiveBeverage("Reactive Guinness"));
+        return Uni.createFrom()
+                .item(new ReactiveBeverage("Reactive Guinness"))
+                .onItem().delayIt()
+                .onExecutor(ContextAwareScheduler.delegatingTo(Infrastructure.getDefaultWorkerPool()).withCurrentContext())
+                .by(Duration.ofSeconds(3));
     }
 }

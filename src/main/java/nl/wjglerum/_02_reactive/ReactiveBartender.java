321c7755aab8520f3b61fe0baff77bead227a653
Willem Jan Glerum <wjglerum@gmail.com>
2025-11-06 08:05:40 +0100

    Remove delay and workaround for reactive

--- src/main/java/nl/wjglerum/_02_reactive/ReactiveBartender.java
+++ src/main/java/nl/wjglerum/_02_reactive/ReactiveBartender.java
@@ -4,17 +4,11 @@ import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 
-import java.time.Duration;
-
 @ApplicationScoped
 public class ReactiveBartender {
 
     public Uni<ReactiveBeverage> getFromDraft() {
         Log.info("Going to pour a reactive Guinness");
-        return Uni.createFrom()
-                .item(new ReactiveBeverage("Reactive Guinness"))
-                .onItem()
-                .delayIt()
-                .by(Duration.ofSeconds(3));
+        return Uni.createFrom().item(new ReactiveBeverage("Reactive Guinness"));
     }
 }
